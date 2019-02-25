a37157ffe5583ed9f74354d1687ba35d110e335d
Brent Goodrick <bgoodr@gmail.com>
2019-02-24 19:41:14 -0800

    Debugging: Add an early return from storeMediaItems when mediaItems is null
    An experiment per discussion in
    
    https://github.com/jonagh/gapi-querier/issues/2

--- commands/findOutOfAlbumPhotos.js
+++ commands/findOutOfAlbumPhotos.js
@@ -4,6 +4,10 @@ const _mediaItems = {};
 
 function storeMediaItems(mediaItems) {
 	if (!mediaItems) {
+		console.log('storeMediaItems : !mediaItems so returning'); // 1681b41d-7207-4049-81f2-98dc0b99f9f8 debug code to be deleted later
+		return;
+	}
+	if (!mediaItems) {
 		console.log('storeMediaItems : !mediaItems'); // 1681b41d-7207-4049-81f2-98dc0b99f9f8 debug code to be deleted later
 	} else if (typeof mediaItems == 'undefined') {
 		console.log('storeMediaItems : typeof mediaItems is undefined'); // 1681b41d-7207-4049-81f2-98dc0b99f9f8 debug code to be deleted later
